454423ddaed7eac9b87a2afee439378600a75744
Priyanka-Thapa <tc.priyanka05@gmail.com>
2025-02-27 15:48:55 +0530

    created components for project

---
+++ src/app/business-module/cards-service/cards-service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-service',
+  imports: [],
+  templateUrl: './cards-service.component.html',
+  styleUrl: './cards-service.component.scss'
+})
+export class CardsServiceComponent {
+
+}
